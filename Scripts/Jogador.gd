8420b04c1bb3dc8a57b4a2e89701fd8501d47823
Rodrigo Pereira <rapieregridoro@gmail.com>
2020-12-15 10:09:34 -0300

    fino do fino do movimento em grade, é o teorico mesmo, agora precisa deixar a função um pouco mais customizavel por fontes externas

--- Scripts/Jogador.gd
+++ Scripts/Jogador.gd
@@ -1,7 +1,8 @@
-extends KinematicBody2D
+extends Node2D
 
 export(int, 1, 2) var Jogador = 1
 var dirbuffer := Vector2(0,0)
+
 var indopara := Vector2(0,0)
 var arena = []
 
@@ -12,40 +13,34 @@ func _ready():
 	
 	arena = SE_Global.Onda[SE_Global.Atual]
 	
-	pos_buffer = position
-	pos_alvo = position
+	pos_original = position
+	pos_final = position
 	
 
 func _physics_process(delta):
 	
 	animation()
-	moviment(delta)
 	
-
-
-func moviment(delta):
-	
-	#excluindo a possibilidade de: diagonais
 	if SE_Global.direcao.x * SE_Global.direcao.y == 0:
 		dirbuffer = SE_Global.direcao
-	
-	if (position - pos_alvo).length_squared() < 5:
-		position = pos_alvo
-		pos_buffer = position
-		pos_alvo = pos_buffer + dirbuffer * 56
-		indopara = dirbuffer
-	
-	
-	print((position - pos_alvo).length_squared())
-	move_and_slide((pos_alvo - pos_buffer) * 5)
-	#position = position + (pos_alvo - pos_buffer)*delta
-	
-	print(dirbuffer)
-	
-	pass
+	movimento_em_grade()
 
-var pos_buffer : Vector2
-var pos_alvo : Vector2
+
+#delta posição
+var pos_original : Vector2
+var pos_final : Vector2
+
+func movimento_em_grade():
+	#movimenta em Grade
+	if (position - pos_final).length_squared() < 5:
+		position = pos_final
+		pos_original = position
+		pos_final = position + dirbuffer * 56
+	
+	position = position + (pos_final - pos_original) * 0.1
+	
+
+
 
 func animation():
 	
@@ -63,4 +58,35 @@ func animation():
 		get_node(String("Gato_" + String(Jogador) ) ).animation = "AndarDireita"
 	
 
+
+
+func moviment():
+#
+#	#excluindo a possibilidade de: diagonais
+#	if SE_Global.direcao.x * SE_Global.direcao.y == 0:
+#		dirbuffer = SE_Global.direcao
+#
+#	if (position - pos_final).length_squared() < 5:
+#
+#		#ao final teleporta para a posição alvo
+#		position = pos_final
+#
+#		#movimentar entre 
+#		#pos_buffer = position
+#		#pos_alvo = pos_buffer + dirbuffer * 56
+#		pos_final = position + dirbuffer * 56
+#
+#		#para a animação
+#		indopara = dirbuffer
+#
+#	else:
+#		position = position.move_toward((pos_final), 5)
+#
+#
+#	#outros metodos que eram promissores:
+#	#	move_and_slide((pos_alvo - pos_buffer) * 5)
+#	#	position = position + (pos_alvo - pos_buffer)*delta
+#
+	pass
+#	
 
